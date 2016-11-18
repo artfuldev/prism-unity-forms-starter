84683040278b5ed9356ff6de97b1cc89a2c0c1a5
Sudarsan Balaji <Sudarsan.Balaji@outlook.com>
2016-11-18 23:46:07 +0530

    Check in starter solution
    Only for Droid, iOS and UWP platforms

---
+++ Prism.Unity.Forms.Starter/Prism.Unity.Forms.Starter/App.xaml.cs
@@ -0,0 +1,22 @@
+﻿using Prism.Unity;
+using Prism.Unity.Forms.Starter.Views;
+
+namespace Prism.Unity.Forms.Starter
+{
+    public partial class App : PrismApplication
+    {
+        public App(IPlatformInitializer initializer = null) : base(initializer) { }
+
+        protected override void OnInitialized()
+        {
+            InitializeComponent();
+
+            NavigationService.NavigateAsync("MainPage?title=Hello%20from%20Xamarin.Forms");
+        }
+
+        protected override void RegisterTypes()
+        {
+            Container.RegisterTypeForNavigation<MainPage>();
+        }
+    }
+}
---
+++ Prism.Unity.Forms.Starter/Prism.Unity.Forms.Starter/Views/MainPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace Prism.Unity.Forms.Starter.Views
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
